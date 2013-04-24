ba9c4e4035746b39291f5556346528760de5b903
Tae Won Ha <qvacua@gmail.com>
2013-04-24 21:22:23 +0200

    keeping the scroll position when reloading

--- Markdowner/MPDocumentWindowController.m
+++ Markdowner/MPDocumentWindowController.m
@@ -9,11 +9,24 @@
 #import <WebKit/WebKit.h>
 #import "MPDocumentWindowController.h"
 
-@implementation MPDocumentWindowController {
+@interface MPDocumentWindowController ()
+
+@property CGFloat pageYOffset;
+@property CGFloat pageXOffset;
+
+@end
+
+@implementation MPDocumentWindowController
+
+- (void)webView:(WebView *)sender didFinishLoadForFrame:(WebFrame *)frame {
+    [self.webView stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"window.scrollTo(%f, %f)", self.pageXOffset, self.pageYOffset]];
 }
 
 #pragma mark Public
 - (void)updateWebView {
+    self.pageYOffset = [self.webView stringByEvaluatingJavaScriptFromString:@"window.pageYOffset"].floatValue;
+    self.pageXOffset = [self.webView stringByEvaluatingJavaScriptFromString:@"window.pageXOffset"].floatValue;
+
     NSString *pathWoLastComponent = [[self.document fileURL].path stringByDeletingLastPathComponent];
     [self.webView.mainFrame loadHTMLString:self.html baseURL:[NSURL URLWithString:pathWoLastComponent]];
 }
@@ -21,6 +34,10 @@
 #pragma mark NSWindowController
 - (void)windowDidLoad {
     [super windowDidLoad];
+
+    self.pageYOffset = 0;
+    self.pageXOffset = 0;
+
     [self updateWebView];
 }
 
